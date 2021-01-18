050baf80da2c4a168798ad6276a29bf4bbd988e6
Stephen <stephenjsweets@gmail.com>
2021-01-18 16:37:59 -0500

    commit tree 7c6ac0ff5612b3ef85ae481211b2b4e5c1a12510

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Stephen"
+    end
+
+    get '/hometown' do
+        "My hometown is Derry"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Cheeseburger in Paradise"
+    end
 end
